a728102495e4570d29e77cce02b39d5205ad6fe4
Manfred Riem <mriem@manorrock.com>
2018-07-02 07:44:04 -0600

    Fixes issue #12 - Add ActivationConfigProperty annotation

--- flounder/src/main/java/javax/ejb/ActivationConfigProperty.java
+++ flounder/src/main/java/javax/ejb/ActivationConfigProperty.java
@@ -25,41 +25,24 @@
  */
 package javax.ejb;
 
-import static java.lang.annotation.ElementType.METHOD;
-import static java.lang.annotation.ElementType.TYPE;
-import java.lang.annotation.Retention;
-import static java.lang.annotation.RetentionPolicy.RUNTIME;
-import java.lang.annotation.Target;
-import java.util.concurrent.TimeUnit;
-import static java.util.concurrent.TimeUnit.MILLISECONDS;
-
 /**
- * The AccessTimeout annotation.
+ * The ActivationConfigProperty annotation.
  * 
  * @author Manfred Riem (mriem@manorrock.com)
  */
-@Target(value={METHOD,TYPE})
-@Retention(value=RUNTIME)
-public @interface AccessTimeout {
-    
+public @interface ActivationConfigProperty {
+
     /**
-     * The time unit.
-     * 
-     * @return the time unit.
+     * Defines the property name.
+     *
+     * @return the property name.
      */
-    TimeUnit unit() default MILLISECONDS;
-    
+    String propertyName();
+
     /**
-     * The access timeout in the unit specified by unit.
-     * 
-     * <ul>
-     *  <li>Value > 0 is the access timeout specified by the unit element.</li>
-     *  <li>Value == 0 no concurrent access allowed.</li>
-     *  <li>Value -1 block indefinitely until finished.</li>
-     *  <li>No other values allowed.</li>
-     * </ul>
-     * 
-     * @return the access timeout.
+     * Defines the property value.
+     *
+     * @return the property value.
      */
-    long value();
+    String propertyValue();
 }
