039638c4dd0bc423bc7b0d609aa17c233f7a91ca
Manfred Riem <mriem@manorrock.com>
2018-07-03 16:06:08 -0600

    Fixes issue #13 - Add AfterBegin annotation

--- flounder/src/main/java/javax/ejb/AfterBegin.java
+++ flounder/src/main/java/javax/ejb/AfterBegin.java
@@ -25,41 +25,10 @@
  */
 package javax.ejb;
 
-import static java.lang.annotation.ElementType.METHOD;
-import static java.lang.annotation.ElementType.TYPE;
-import java.lang.annotation.Retention;
-import static java.lang.annotation.RetentionPolicy.RUNTIME;
-import java.lang.annotation.Target;
-import java.util.concurrent.TimeUnit;
-import static java.util.concurrent.TimeUnit.MILLISECONDS;
-
 /**
- * The AccessTimeout annotation.
+ * The AfterBegin annotation.
  * 
  * @author Manfred Riem (mriem@manorrock.com)
  */
-@Target(value={METHOD,TYPE})
-@Retention(value=RUNTIME)
-public @interface AccessTimeout {
-    
-    /**
-     * The time unit.
-     * 
-     * @return the time unit.
-     */
-    TimeUnit unit() default MILLISECONDS;
-    
-    /**
-     * The access timeout in the unit specified by unit.
-     * 
-     * <ul>
-     *  <li>Value &gt; 0 is the access timeout specified by the unit element.</li>
-     *  <li>Value == 0 no concurrent access allowed.</li>
-     *  <li>Value -1 block indefinitely until finished.</li>
-     *  <li>No other values allowed.</li>
-     * </ul>
-     * 
-     * @return the access timeout.
-     */
-    long value();
+public @interface AfterBegin {
 }
