c00d8f65163fd266413a42915df1931985a3c3e1
Manfred Riem <mriem@manorrock.com>
2019-08-28 16:11:50 -0600

    Fixes issue #81 - Update license headers to BSD3

--- flounder-ejb/src/main/java/javax/ejb/EJBTransactionRolledbackException.java
+++ flounder-ejb/src/main/java/javax/ejb/EJBTransactionRolledbackException.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright (c) 2002-2018, Manorrock.com. All Rights Reserved.
+ *  Copyright (c) 2002-2019, Manorrock.com. All Rights Reserved.
  *
  *  Redistribution and use in source and binary forms, with or without
  *  modification, are permitted provided that the following conditions are met:
@@ -10,6 +10,10 @@
  *      2. Redistributions in binary form must reproduce the above copyright
  *         notice, this list of conditions and the following disclaimer in the
  *         documentation and/or other materials provided with the distribution.
+ *
+ *      3. Neither the name of the copyright holder nor the names of its 
+ *         contributors may be used to endorse or promote products derived from
+ *         this software without specific prior written permission.
  *
  *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  *  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
